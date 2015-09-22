019933d7c1b9e0159258e26c0533cb810fdaf103
Mitch Lillie <mitch@mitchlillie.com>
2015-09-22 16:50:45 -0700

    task views and user/unassigned views

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ var app = {};
 
 $(function() { //when DOM is ready...
 	app.users = new UserCollection([
-		{username:'Person1'},
-		{username:'Person2'},
-		{username:'Person3'}
+		{username:'do'},
+		{username:'jesse'},
+		{username:'mitch'}
 	]);
 
 	app.tasks = new TaskCollection([
+		{
+			title: 'finish project',
+			description: "let's figure out backbone goddammit",
+			creator: 'mitch',
+			assignee: '',
+			status: 'unassigned'
+		},
+		{
+			title: 'eat a snack',
+			description: "healthy one",
+			creator: 'do',
+			assignee: 'do',
+			status: 'New'
+		}
 		// test data here
 	]);
+
 
 	app.gui = new GUI(app.users,
 						app.tasks,
 						'#app');// selector of main div
-}
+});
--- models.js
+++ models.js
@@ -2,23 +2,48 @@ var UserModel = Backbone.Model.extend({
 	defaults: {
 		username:''
 	}
-})
+});
 
-var IssueModel = Backbone.Model.extend({
+var TaskModel = Backbone.Model.extend({
 	defaults: {
 		title:'',
 		description:'',
 		creator:'',
 		assignee:'',
 		status:'unassigned',
+	},
+	assign: function (newUser) {
+		//Change assignment to another person
+		this.set('assignee', newUser);
+	},
+	statusUpdate: function (newStatus) {
+		//Set new status
+		this.set('status', newStatus);
+	},
+	editTitle: function (newTitle) {
+		this.set('title', newTitle);
+		//Update title
+	},
+	editDescription: function (newDesc) {
+		this.set('description', newDesc);
+		//Update desc
 	}
 	// Add methods if needed...
-})
+	//TODO: Copy this
+	// var TextField = Backbone.Model.extend({
+	//   defaults: {"string": ""},
+	//   add: function (newText) {
+	//     var str = this.get("string");
+	//     this.set("string", str + " " + newText);
+	//   }
+	// });
+
+});
 
 var UserCollection = Backbone.Collection.extend({
 	model:UserModel
-})
+});
 
-var IssueCollection = Backbone.Collection.extend({
-	model:IssueModel
-})
+var TaskCollection = Backbone.Collection.extend({
+	model:TaskModel
+});
